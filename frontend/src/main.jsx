0b6ff8e9613740e7e8426abbd7255a9b74c3384f
Sayan Sanpui <sayansanpui@Sayans-MacBook-Pro.local>
2024-11-22 17:47:45 +0530

    homepage UI  complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
 )
