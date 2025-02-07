45d30704d8034076c45233749c49fc9f27efdb85
sayansanpui <sayansanpuicoding@gmail.com>
2025-02-08 00:45:05 +0530

    Frontend Complete

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -7,7 +7,7 @@ const NewsletterBox = () => {
 
     return (
         <div className=' text-center'>
-            <p className=' text-2xl font-medium text-green-800'>Subscribe now & get 20% off</p>
+            <p className=' text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
             <p className=' text-gray-400 mt-3'>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae 
             </p>
